22ec591a728002ef34dcb335d0a5e1f98f52b528
SantosRojas <santos777rojas@gmail.com>
2023-09-09 02:30:08 -0500

    add hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {HashRouter as Router, Routes, Route } from 'react-router-dom';
 import VistaEdit from './pages/Edit';
 import Register from './pages/Register';
 import { useState } from 'react';
